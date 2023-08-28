627086b3e8d87aada5d559d9e5e8d597e8ac52f2
Tristan <heytristaann@gmail.com>
2023-08-28 07:31:03 -0400

    added more rooms

--- app/lib/constants/rooms.ts
+++ app/lib/constants/rooms.ts
@@ -13,18 +13,73 @@ export const ROOM_INFOS: { [key: string]: RoomInfo } = {
   },
   CRSC116: {
     type: "Classroom",
-    seating: "Loose Chairs and Tables",
+    seating: "Loose Chairs, Loose Tables",
     capacity: 70,
   },
   CRSC117: {
     type: "Classroom",
-    seating: "Loose Chairs and Tables",
+    seating: "Loose Chairs, Loose Tables",
     capacity: 70,
   },
   CRSC101: {
     type: "Classroom",
-    seating: "Loose Chairs and Tables",
+    seating: "Loose Chairs, Loose Tables",
     capacity: 20,
+  },
+  CRSC403: {
+    type: "Classroom",
+    seating: "Loose Chairs, Loose Tables",
+    capacity: 31,
+  },
+  ANNU156: {
+    type: "Lecture Theatre",
+    seating: "Fixed Chairs with Tablets",
+    capacity: 99,
+  },
+  ANNU204: {
+    type: "Classroom",
+    seating: "Fixed Chairs with Tablets",
+    capacity: 48,
+  },
+  ANNU306: {
+    type: "Classroom",
+    seating: "Fixed Chairs with Tablets",
+    capacity: 48,
+  },
+  FS241: {
+    type: "Case Study",
+    seating: "Loose Chairs, Fixed Tablets",
+    capacity: 60,
+  },
+  MAC149: {
+    type: "Lecture Theatre",
+    seating: "Fixed Chairs with Tablets",
+    capacity: 200,
+  },
+  MACS121: {
+    type: "Case Study",
+    seating: "Fixed Chairs, Fixed Tablets",
+    capacity: 60,
+  },
+  MACS129: {
+    type: "Lecture Theatre",
+    seating: "Loose Chairs, Fixed Tablets",
+    capacity: 40,
+  },
+  MCLN101: {
+    type: "Case Study",
+    seating: "Loose Chairs with Tablets",
+    capacity: 40,
+  },
+  MCLN102: {
+    type: "Lecture Theatre",
+    seating: "Fixed Chairs with Tablets",
+    capacity: 160,
+  },
+  MCLN107: {
+    type: "Case Study",
+    seating: "Loose Chairs Fixed Tablets",
+    capacity: 60,
   },
   WMEM: {
     type: "Lecture Theatre",
