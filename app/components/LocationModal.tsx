b0eed8ffe45ec78da8d7c6d45f62dcdd1b492212
Tristan <heytristaann@gmail.com>
2023-09-06 08:42:16 -0400

    cleaned up

--- app/components/LocationModal.tsx
+++ app/components/LocationModal.tsx
@@ -19,13 +19,13 @@ export default function LocationModal(props: LocationModalProps) {
       role="dialog"
       tabIndex={-1}
     >
-      <div className="relative flex h-72 w-72 flex-col items-center justify-center rounded-lg bg-slate-900 p-4 xs:h-80 xs:w-80 xs:p-8 sm:h-[28rem] sm:w-[28rem]">
+      <div className="relative flex h-72 w-72 flex-col gap-4 rounded-lg bg-slate-900 p-4 xs:h-80 xs:w-80 xs:p-8 sm:h-[28rem] sm:w-[28rem] sm:gap-6">
         <Image
           src={props.activeStep.value.image}
           alt="..."
           width={1920}
           height={1080}
-          className="mb-6 h-52 w-72 rounded-lg sm:h-80 sm:w-96"
+          className="h-52 w-72 rounded-lg sm:h-80 sm:w-96"
         />
         <p className="text-center text-xs font-normal tracking-wider text-white xs:text-sm sm:text-base">
           <mark className="mr-2 bg-transparent bg-gradient-to-br from-blue-600 to-violet-700 bg-clip-text font-semibold tracking-wide text-transparent">
